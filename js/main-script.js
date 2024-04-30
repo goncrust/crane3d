9a50c9b35b303513471f93d021f50504ff9756ef
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2024-04-30 12:27:09 +0100

    Create cameras
    Lateral, Frontal, Top, BroadPerspective, BroadOrthographic and Claw

--- js/main-script.js
+++ js/main-script.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { VRButton } from 'three/addons/webxr/VRButton.js';
-import * as Stats from 'three/addons/libs/stats.module.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { VRButton } from "three/addons/webxr/VRButton.js";
+import * as Stats from "three/addons/libs/stats.module.js";
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 
 //////////////////////
 /* GLOBAL VARIABLES */
@@ -13,29 +13,109 @@ var camera, scene, renderer;
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene(){
-    'use strict';
+function createScene() {
+    "use strict";
 
     scene = new THREE.Scene();
 
     scene.add(new THREE.AxesHelper(1));
-
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createCameras() {
+    createLateralCamera();
+    createFrontalCamera();
+    createTopCamera();
+    createClawCamera();
+    createBroadPerpectiveCamera();
+    createBroadOrthographicCamera();
+}
 
-function createCamera(){
-    'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-            window.innerWidth / window.innerHeight,
-            1,
-            1000);
+function createLateralCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    camera.position.x = 0;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(0, 50, 0);
+}
+
+function createFrontalCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    camera.position.x = 100;
+    camera.position.y = 50;
+    camera.position.z = 0;
+    camera.lookAt(0, 50, 0);
+}
+
+function createTopCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    camera.position.x = 0;
+    camera.position.y = 100;
+    camera.position.z = 0;
+    camera.lookAt(0, 0, 0);
+}
+
+function createClawCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    // TODO
     camera.position.x = 50;
     camera.position.y = 50;
     camera.position.z = 50;
     camera.lookAt(scene.position);
+}
+
+function createBroadPerpectiveCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    camera.position.x = 75;
+    camera.position.y = 75;
+    camera.position.z = 75;
+    camera.lookat(0, 50, 0);
+}
+
+function createBroadOrthographicCamera() {
+    "use strict";
+    camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000,
+    );
+    camera.position.x = 75;
+    camera.position.y = 75;
+    camera.position.z = 75;
+    camera.lookat(0, 50, 0);
 }
 
 /////////////////////
@@ -49,83 +129,75 @@ function createCamera(){
 //////////////////////
 /* CHECK COLLISIONS */
 //////////////////////
-function checkCollisions(){
-    'use strict';
-
+function checkCollisions() {
+    "use strict";
 }
 
 ///////////////////////
 /* HANDLE COLLISIONS */
 ///////////////////////
-function handleCollisions(){
-    'use strict';
-
+function handleCollisions() {
+    "use strict";
 }
 
 ////////////
 /* UPDATE */
 ////////////
-function update(){
-    'use strict';
-
+function update() {
+    "use strict";
 }
 
 /////////////
 /* DISPLAY */
 /////////////
 function render() {
-    'use strict';
+    "use strict";
     renderer.render(scene, camera);
-
 }
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
 function init() {
-    'use strict';
+    "use strict";
     renderer = new THREE.WebGLRenderer({
-        antialias: true
+        antialias: true,
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createCamera();
+    createCameras();
 
     render();
-
 }
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
 function animate() {
-    'use strict';
-
+    "use strict";
 }
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() { 
-    'use strict';
-
+function onResize() {
+    "use strict";
 }
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
 ///////////////////////
 function onKeyDown(e) {
-    'use strict';
-
+    "use strict";
 }
 
 ///////////////////////
 /* KEY UP CALLBACK */
 ///////////////////////
-function onKeyUp(e){
-    'use strict';
+function onKeyUp(e) {
+    "use strict";
 }
 
 init();
